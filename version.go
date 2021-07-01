fa9221b8677f7c2b87ac8087977c96c357c34371
João Pedro Lima Rufino <joao.rufino@ua.pt>
2021-07-01 12:20:10 +0100

    testing fake commits

--- version.go
+++ version.go
@@ -1,9 +1,9 @@
 package main
 
-// Version stores the current package semantic version
+//  Version stores the current package semantic version
 var Version = "dev"
 
-// Versions represents the used versions for several significant dependencies
+//  Versions represents the used versions for several significant dependencies
 type Versions struct {
 	ImaginaryVersion string `json:"imaginary"`
 	BimgVersion      string `json:"bimg"`
